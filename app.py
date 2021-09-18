330d88a71a5eb57325266adb32a6b836d377ecbb
Juan <jdteheran@uninorte.edu.co>
2021-09-17 20:39:29 -0500

    elimine codigo

--- app.py
+++ app.py
@@ -5,16 +5,9 @@ for i in range(1, numero + 1):
     if numero % i == 0:
         cont = cont + 1
 
-if cont == 2:
-    print('el numero: ', numero, ' es primo') 
-else:
-    print('el numero: ', numero, ' NO es primo') 
 
 
 cont = 0
-for i in range(1, numero + 1):
-    if numero % i == 0:
-        cont = cont + 1
 
 if cont == 2:
     print('el numero: ', numero, ' es primo') 
