cb8f9fa74debc88919064ec1f7c864d32afec894
Juan <jdteheran@uninorte.edu.co>
2021-09-17 20:12:25 -0500

    no he podido solucionar esto

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 numero = input('pasame un numero: ')
 
-print('el numero es: ', numero)
+cont = 0
+for i in range(numero):
+    if numero % i == 0:
+        cont = cont + 1
+
+if cont == 2:
+    print('el numero: ', numero, ' es primo') 
+else:
+    print('el numero: ', numero, ' NO es primo') 
